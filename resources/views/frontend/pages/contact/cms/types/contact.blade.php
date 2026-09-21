8ff9e16e23d2065e17ddf3ce2d9702f9d643a5b0
KareemShaban1 <karim17196@fci.bu.edu.eg>
2026-09-21 11:47:47 +0300

    change location in map

--- resources/views/frontend/pages/contact/cms/types/contact.blade.php
+++ resources/views/frontend/pages/contact/cms/types/contact.blade.php
@@ -258,8 +258,9 @@ $normalized = preg_replace('#<br\s* /?>#i', "\n", (string) $content);
 
 	<div class="">
 		<div class="contact-map">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3644.7310056272386!2d89.2286059153658!3d24.00527418490799!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fe9b97badc6151%3A0x30b048c9fb2129bc!2sAngfuztheme!5e0!3m2!1sen!2sbd!4v1651028958211!5m2!1sen!2sbd"
-				allowfullscreen="" loading="lazy"></iframe>
+			<iframe src="https://www.google.com/maps?q=24.4628363,39.5779308&z=17&hl=en&output=embed"
+				title="{{ __('main.address_text') }}"
+				allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 			<div class="contact-icon">
 				<img src="{{ asset('frontend/assets/img/icon/location-dot.svg') }}" alt="" decoding="async" loading="lazy">
 			</div>
